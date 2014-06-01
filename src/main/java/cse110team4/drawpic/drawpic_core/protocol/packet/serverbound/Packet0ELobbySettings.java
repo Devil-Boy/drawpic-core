7f31335f7fd4d35c10691571ee4dc6745adc6134
Kirk <kkw013@ucsd.edu>
2014-05-31 21:23:14 -0700

    Created LobbySettings Packet

---
+++ src/main/java/cse110team4/drawpic/drawpic_core/protocol/packet/serverbound/Packet0ELobbySettings.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package cse110team4.drawpic.drawpic_core.protocol.packet.serverbound;
+
+import cse110team4.drawpic.drawpic_core.LobbySettings;
+import cse110team4.drawpic.drawpic_core.protocol.StreamReadException;
+import cse110team4.drawpic.drawpic_core.protocol.StreamReader;
+import cse110team4.drawpic.drawpic_core.protocol.StreamWriteException;
+import cse110team4.drawpic.drawpic_core.protocol.StreamWriter;
+import cse110team4.drawpic.drawpic_core.protocol.packet.Packet;
+
+/**
+ * Packet to send the lobby settings to the server
+ * @author Kirk
+ *
+ */
+public class Packet0ELobbySettings extends Packet {
+
+	final static byte LOBBY_SETTINGS = 0x0E;
+	LobbySettings settings;
+	
+	/**
+	 * Default constructor
+	 */
+	public Packet0ELobbySettings() {
+		super(LOBBY_SETTINGS);
+	}
+
+	/**
+	 * Constructor to store settings
+	 * @param settings to pass in Lobbysettings to be sent
+	 */
+	public Packet0ELobbySettings(LobbySettings settings){
+		super(LOBBY_SETTINGS);
+		this.settings = settings;
+	}
+	@Override
+	public void readFromStream(StreamReader reader) throws StreamReadException {
+		// Not needed
+
+	}
+
+	@Override
+	public void writeBodyToStream(StreamWriter writer) throws StreamWriteException {
+		settings.writeToStream(writer);
+
+	}
+
+}
