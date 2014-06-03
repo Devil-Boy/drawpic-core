56caafe7e4cb7eead981eef7077d4c3be7373ea3
Devil Boy <devilboy@pinoygamers.info>
2014-06-03 16:16:36 -0700

    Made packet0A read lobbies properly

--- src/main/java/cse110team4/drawpic/drawpic_core/protocol/packet/serverbound/Packet0AJoinLobby.java
+++ src/main/java/cse110team4/drawpic/drawpic_core/protocol/packet/serverbound/Packet0AJoinLobby.java
@@ -1,6 +1,8 @@
 package cse110team4.drawpic.drawpic_core.protocol.packet.serverbound;
 
+import cse110team4.drawpic.drawpic_core.CoreBeans;
 import cse110team4.drawpic.drawpic_core.player.Lobby;
+import cse110team4.drawpic.drawpic_core.player.LobbyParser;
 import cse110team4.drawpic.drawpic_core.protocol.StreamReadException;
 import cse110team4.drawpic.drawpic_core.protocol.StreamReader;
 import cse110team4.drawpic.drawpic_core.protocol.StreamWriteException;
@@ -43,13 +45,12 @@ public class Packet0AJoinLobby extends Packet{
 	
 	@Override
 	public void readFromStream(StreamReader reader) throws StreamReadException {
-		// Not needed
-		
+		lobby = CoreBeans.getContext().getBean(LobbyParser.class).fromStream(reader);
 	}
+	
 	@Override
 	public void writeBodyToStream(StreamWriter writer) throws StreamWriteException {
 		lobby.writeToStream(writer);
-		
 	}
 
 }
