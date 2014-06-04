517c2d0dd812ce5413deaacb177f60eacb3b5901
cinnamonbagels <samuelsko@gmail.com>
2014-06-04 15:26:59 -0700

    pushing in more changes to testers

--- src/test/java/cse110team4/drawpic/drawpic_core/player/TestGameData.java
+++ src/test/java/cse110team4/drawpic/drawpic_core/player/TestGameData.java
@@ -1,5 +1,37 @@
 package cse110team4.drawpic.drawpic_core.player;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cse110team4.drawpic.drawpic_core.gamesession.GamePhase;
+
 public class TestGameData {
+	@Test
+	public void testGameData() {
+		GameData data = new GameData();
+		
+		if(data.getPhase() == null) {
+			//pass
+		} else {
+			fail();
+		}
+		
+		if(data.getJudge() == null) {
+			//pass	
+		} else {
+			fail();
+		}
+		
+		data.setPhase(GamePhase.DRAW_PHASE);
+		
+		assertEquals("", GamePhase.DRAW_PHASE, data.getPhase());
+		
+		data.setJudge("huehue");
+		
+		assertEquals("","huehue", data.getJudge());
+		
+		
+	}
 
 }
