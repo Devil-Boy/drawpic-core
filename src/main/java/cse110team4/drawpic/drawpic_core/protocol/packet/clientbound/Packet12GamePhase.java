109ab455fe3e407e0ad3f65b4ee358246ad82ef3
Devil Boy <devilboy@pinoygamers.info>
2014-06-04 15:39:39 -0700

    Fixed up game phase packet

--- src/main/java/cse110team4/drawpic/drawpic_core/protocol/packet/clientbound/Packet12GamePhase.java
+++ src/main/java/cse110team4/drawpic/drawpic_core/protocol/packet/clientbound/Packet12GamePhase.java
@@ -1,5 +1,6 @@
 package cse110team4.drawpic.drawpic_core.protocol.packet.clientbound;
 
+import cse110team4.drawpic.drawpic_core.gamesession.GamePhase;
 import cse110team4.drawpic.drawpic_core.protocol.StreamReadException;
 import cse110team4.drawpic.drawpic_core.protocol.StreamReader;
 import cse110team4.drawpic.drawpic_core.protocol.StreamWriteException;
@@ -9,33 +10,29 @@ import cse110team4.drawpic.drawpic_core.protocol.packet.Packet;
 public class Packet12GamePhase extends Packet {
 	final static byte GAME_PHASE_ID = 0x12;
 	
-	String phase;
+	GamePhase phase;
 
 	public Packet12GamePhase() {
 		super(GAME_PHASE_ID);
 	}
 	
-	public String getPhase() {
+	public Packet12GamePhase(GamePhase phase) {
+		super(GAME_PHASE_ID);
+		
+		this.phase = phase;
+	}
+	
+	public GamePhase getPhase() {
 		return phase;
 	}
 
-	public void setPhase(String phase) {
-		this.phase = phase;
-	}
-
-	public Packet12GamePhase(String s) {
-		super(GAME_PHASE_ID);
-		
-		phase = s;
-	}	
-
 	@Override
 	public void readFromStream(StreamReader reader) throws StreamReadException {
-		phase = reader.readString();
+		phase = GamePhase.valueOf(reader.readString());
 	}
 
 	@Override
 	public void writeBodyToStream(StreamWriter writer) throws StreamWriteException {
-		writer.writeString(phase);
+		writer.writeString(phase.name());
 	}
 }
